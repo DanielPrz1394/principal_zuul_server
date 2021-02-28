cb3a0374d9deb9b4015abbd5faca2466c8516a40
Cesar Daniel Perez Olvera <cesar.olvera@mail.telcel.com>
2021-02-28 17:18:43 -0600

    C# 2020/02/28 Generación y configuración inicial del proyecto.
    * Generación del proyecto.
    * Configuración de propiedades.

---
+++ src/test/java/com/github/danielprz1394/principal_zuul_server/PrincipalZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.danielprz1394.principal_zuul_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincipalZuulServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
